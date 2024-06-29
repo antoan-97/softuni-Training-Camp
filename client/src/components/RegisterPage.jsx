48865a506fa911214205dbaaad4cff582e0f1d19
antoan-97 <antoan.todorov@abv.bg>
2024-06-29 12:06:35 +0300

    Add Register Page component

--- client/src/components/RegisterPage.jsx
+++ client/src/components/RegisterPage.jsx
@@ -1,30 +1,42 @@
+import styles from '../styles/Register.module.css'
+
+
 export default function RegisterPage() {
     return (
         <section id="register-page">
-            <div class="signupSection">
-
-                <form class="signupForm">
-                    <h2>Sign Up</h2>
-                    <ul class="noBullet">
-
-                        <li>
-                            <label for="email">Email:</label>
-                            <input type="text" class="inputFields" id="email" name="email" />
-                        </li>
-                        <li>
-                            <label for="password">Password:</label>
-                            <input type="password" class="inputFields" id="password" name="password" />
-                        </li>
-                        <li>
-                            <label for="repeat-password">Repeat-Password:</label>
-                            <input type="password" class="inputFields" id="repeat-password" name="repeatPassword" />
-                        </li>
-                        <li id="center-btn">
-                            <button id="join-btn">JOIN</button>
-                        </li>
-                    </ul>
-                </form>
-            </div>
-        </section>
+        <div className={styles.signupSection}>
+          <form className={styles.signupForm}>
+            <h2>Sign Up</h2>
+            <ul className={styles.noBullet}>
+              <li>
+                <label htmlFor="email">Email:</label>
+                <input type="text" className={styles.inputFields} id="email" name="email" />
+              </li>
+              <li>
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="password"
+                  className={styles.inputFields}
+                  id="password"
+                  name="password"
+                />
+              </li>
+              <li>
+                <label htmlFor="repeat-password">Repeat-Password:</label>
+                <input
+                  type="password"
+                  className={styles.inputFields}
+                  id="repeat-password"
+                  name="repeatPassword"
+                />
+              </li>
+              <li id="center-btn">
+                <button id={styles['join-btn']}>JOIN</button>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </section>
+      
     );
 }
