5e23ac366aafd7185763ce7839ad24dad4c02e34
antoan-97 <antoan.todorov@abv.bg>
2024-06-29 12:36:40 +0300

    Create LoginPage.jsx

---
+++ client/src/components/LoginPage.jsx
@@ -0,0 +1,38 @@
+import styles from '../styles/Login.module.css'
+
+export default function LoginPage() {
+    return (
+        <section id="login-page" className={styles.loginSection}>
+        <div className={styles.loginContainer}>
+            <form className={styles.loginForm}>
+                <h2>Login</h2>
+                <ul className={styles.listNoBullet}>
+                    <li>
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="text"
+                            className={styles.inputField}
+                            id="email"
+                            name="email"
+                        />
+                    </li>
+                    <li>
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            className={styles.inputField}
+                            id="password"
+                            name="password"
+                        />
+                    </li>
+                    <li className={styles.centeredBtn}>
+                        <button className={styles.loginButton}>Login</button>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </section>
+
+    );
+
+}
