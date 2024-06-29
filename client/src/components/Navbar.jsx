d41444182f8ddd113c874b9cd9573d980eb5eaab
antoan-97 <antoan.todorov@abv.bg>
2024-06-29 11:48:17 +0300

    Use Link in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom'
+
 import styles from '../styles/Navbar.module.css'
 
 export default function Navbar() {
     return (
         <nav className={styles.navbar}>
             <ul className={styles['nav-menu']}>
-                <li><a href="/">Home</a></li>
-                <li><a href="/collection">Collection</a></li>
-                <li><a href="/register">Register</a></li>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/logout">Logout</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/collection">Collection</Link></li>
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/logout">Logout</Link></li>
             </ul>
         </nav>
     );
