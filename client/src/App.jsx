8c1a35935062b7389ac206b36d6cf062cfee90c4
antoan-97 <antoan.todorov@abv.bg>
2024-06-29 11:41:15 +0300

    Implement Routes from react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,5 @@
+import { Routes , Route } from 'react-router-dom'
+
 import styles from './styles/App.module.css'
 
 import Navbar from "./components/Navbar"
@@ -10,6 +12,10 @@ function App() {
   return (
     <div className={styles.background}>
       <Navbar />
+    <Routes>
+      <Route path='/' element={<HomePage />}></Route>
+    </Routes>
+         
       <Footer />
     </div>
   )
