7f7db3aa21a3f24e27df0bb248bc172ac74908e4
antoan-97 <antoan.todorov@abv.bg>
2024-07-03 13:11:31 +0300

    Add createFighterHandler

--- client/src/components/createPage/CreatePage.jsx
+++ client/src/components/createPage/CreatePage.jsx
@@ -1,52 +1,58 @@
 import styles from '../../styles/Create.module.css'
 
-export default function CreatePage() {
-    return (
-        <section id="new-post-page" className={styles.newPostSection}>
+const createFighterHandler = (e) => {
+  e.preventDefault();
+  const fighterData = Object.fromEntries(new FormData(e.currentTarget))
+  console.log(fighterData);
+}
 
-          <form method="POST" className={styles.newPostForm}>
-            <h2>Create New Fighter</h2>
-            <ul className={styles.bulletlessList}>
-              <li>
-                <label htmlFor="name">Fighter Name:</label>
-                <input
-                  type="text"
-                  className={styles.inputField}
-                  id="name"
-                  name="name"
-                />
-              </li>
-              <li>
-                <label htmlFor="category">Category:</label>
-                <input
-                  type="text"
-                  className={styles.inputField}
-                  id="category"
-                  name="category"
-                />
-              </li>
-              <li>
-                <label htmlFor="image">Image URL:</label>
-                <input
-                  type="text"
-                  className={styles.inputField}
-                  id="image"
-                  name="image"
-                />
-              </li>
-              <li>
-                <label htmlFor="description">Description:</label>
-                <textarea
-                  id="description"
-                  className={styles.inputField}
-                  name="description"
-                />
-              </li>
-              <li className={styles.centeredButton}>
-                <button className={styles.createButton}>Create</button>
-              </li>
-            </ul>
-          </form>
-        </section>
-      );
+export default function CreatePage() {
+  return (
+    <section id="new-post-page" className={styles.newPostSection}>
+
+      <form onSubmit={createFighterHandler} className={styles.newPostForm}>
+        <h2>Create New Fighter</h2>
+        <ul className={styles.bulletlessList}>
+          <li>
+            <label htmlFor="name">Fighter Name:</label>
+            <input
+              type="text"
+              className={styles.inputField}
+              id="name"
+              name="title"
+            />
+          </li>
+          <li>
+            <label htmlFor="category">Category:</label>
+            <input
+              type="text"
+              className={styles.inputField}
+              id="category"
+              name="category"
+            />
+          </li>
+          <li>
+            <label htmlFor="image">Image URL:</label>
+            <input
+              type="text"
+              className={styles.inputField}
+              id="image"
+              name="image"
+            />
+          </li>
+          <li>
+            <label htmlFor="description">Description:</label>
+            <textarea
+              id="description"
+              className={styles.inputField}
+              name="description"
+            />
+          </li>
+          <li className={styles.centeredButton}>
+            <button className={styles.createButton}>Create</button>
+          </li>
+        </ul>
+      </form>
+    </section>
+  );
 }
