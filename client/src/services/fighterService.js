afe6249e7603fc60efbe89266612e353c1a012d2
antoan-97 <antoan.todorov@abv.bg>
2024-07-08 17:47:29 +0300

    Add get all service method

--- client/src/services/fighterService.js
+++ client/src/services/fighterService.js
@@ -1,8 +1,10 @@
-const baseUrl = 'http://localhost:3030/jsonstore'
+import { json } from "react-router-dom";
+
+const baseUrl = 'http://localhost:3030/jsonstore/fighters'
 
 export const create = async (fighterData) => {
 
-    const resposne = await fetch(`${baseUrl}/fighters`, {
+    const resposne = await fetch(baseUrl, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
@@ -13,4 +15,23 @@ export const create = async (fighterData) => {
     const result = await resposne.json();
 
     return result;
+}
+
+
+export const getAll = async () =>{
+    const response = await fetch(baseUrl, {
+        method: 'GET',
+        headers: {
+            'content-type' : 'application/json'
+        },
+    })
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok' + response.statusText);
+    }
+
+    const result = await response.json()
+   
+
+    return Object.values(result)
 }
