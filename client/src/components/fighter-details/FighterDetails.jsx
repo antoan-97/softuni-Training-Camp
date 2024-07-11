2193adbb7036eec1167b7d17b4df8632022fba56
antoan-97 <antoan.todorov@abv.bg>
2024-07-11 15:56:34 +0300

    Create basic details component

---
+++ client/src/components/fighter-details/FighterDetails.jsx
@@ -0,0 +1,51 @@
+export default function ({
+    title,
+    description
+}) {
+    return (
+
+
+        <section id="details-page">
+
+            <div className="main_card">
+                <div className="card_left">
+                    <div className="card_datails">
+
+                        <h1>Name: {title}</h1>
+                        <h3>Description: </h3>
+                        <div className="card_fighter">
+                            <p className="wins">Wins: </p>
+                            <p className="loses">Loses: </p>
+                            <p className="weight">Weight: </p>
+                        </div>
+
+                        <p className="disc">Description: </p>
+                        {/* {{ #if user }} */}
+                        <div className="social-btn">
+                            {/* {{ #if isOwner }} */}
+                            {/* <!-- Only for registered user and author of the post --> */}
+                            <a href="/fighters-list/${fighterId}/edit" className="edit-btn">Edit</a>
+                            <a href="/fighter-list/${fighterId}/delete" className="del-btn">Delete</a>
+                            {/* {{ else}}
+                                {{ #if hasSign }}
+                                <!-- logged in user who has already voted--> */}
+                            <p className="thanks-for-sign">Thanks For Signing</p>
+                            {/* {{ else}}
+                                <!-- logged in users, who have not yet voted--> */}
+                            <a href="/fighters-list/${figterId}/signUp" className="sign-up">Sign</a>
+                            {/* {{/if}}
+                                {{/if}} */}
+                        </div>
+                        {/* {{/if}} */}
+                    </div>
+                </div>
+                <div className="card_right">
+                    <img src="" alt="image" />
+                </div>
+            </div>
+
+    
+
+        </section>
+    );
+}
