820136d3de3eba2350a97df8e323336a675f43c8
antoan-97 <antoan.todorov@abv.bg>
2024-07-11 12:38:30 +0300

    Render all fighters

--- client/src/components/fightersList/FightersList.jsx
+++ client/src/components/fightersList/FightersList.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import * as fighterService from '../../services/fighterService'
 
 import styles from '../../styles/Collection.module.css';
+import FighterListItem from './fighterListItem/FighterListItem';
 
 export default function FightersList() {
 
@@ -12,21 +13,20 @@ export default function FightersList() {
         fighterService.getAll()
             .then(result => setFighters(result))
     }, []);
-    
+
     console.log(fighters);
 
 
     return (
-        <div className={`${styles.flip} ${styles.flipVertical}`}>
-            <div className={styles.flipContainerFront}>
-                <img src="" alt="fighter" className={styles.frontImage} />
-            </div>
-            <div className={styles.flipContainerBack}>
-                <h1 className={styles.frontHeading}>Fighter Name</h1>
-                <h2 className={styles.flipContainerBackHeading}>Category: </h2>
-                <p className={styles.flipContainerBackDescription}>Description:  </p>
-                <a href="/fighters/{{_id}}/details" className={styles.detailsButton}>Details</a>
-            </div>
-        </div>
+        <section id="catalog-page">
+
+            <h1>All Fighters</h1>
+
+            {fighters.map(fighter =>  (
+                <FighterListItem {...fighter} />
+            ))}
+
+            <h3 className='no-articles'>No articles yet</h3>
+        </section>
     );
 }
