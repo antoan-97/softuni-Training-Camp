3cf475b0f3e2ca79c9fffecfe66945016b51fdd3
antoan-97 <antoan.todorov@abv.bg>
2024-07-11 12:38:00 +0300

    Create FighterListItem.jsx

---
+++ client/src/components/fightersList/fighterListItem/FighterListItem.jsx
@@ -0,0 +1,23 @@
+import styles from '../../../styles/Collection.module.css'
+export default function FighterListItem({
+    title,
+    category,
+    image,
+    description
+}
+
+) {
+    return (
+        <div className={`${styles.flip} ${styles.flipVertical}`}>
+            <div className={styles.flipContainerFront}>
+                <img src={image} alt="fighter" className={styles.frontImage} />
+            </div>
+            <div className={styles.flipContainerBack}>
+                <h1 className={styles.frontHeading}>Fighter Name: {title}</h1>
+                <h2 className={styles.flipContainerBackHeading}>Category: {category} </h2>
+                <p className={styles.flipContainerBackDescription}>Description: {description}  </p>
+                <a href="/fighters/{{_id}}/details" className={styles.detailsButton}>Details</a>
+            </div>
+        </div>
+    );
+}
