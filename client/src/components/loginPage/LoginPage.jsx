a23cf6db61e37504f4b7e5d9a392332f81c265ae
antoan-97 <antoan.todorov@abv.bg>
2024-07-13 22:02:29 +0300

    Add password error state validation

--- client/src/components/loginPage/LoginPage.jsx
+++ client/src/components/loginPage/LoginPage.jsx
@@ -11,10 +11,10 @@ export default function LoginPage() {
     };
 
 
-    const [formState,setFormState] = useState(initialFormState);
-    const [errorState,setErrorState] = useState('')
+    const [formState, setFormState] = useState(initialFormState);
+    const [errorState, setErrorState] = useState('')
 
-    const formChangeHandler = (e) =>{
+    const formChangeHandler = (e) => {
         setFormState(state => ({
             ...state,
             [e.target.name]: e.target.value,
@@ -23,51 +23,62 @@ export default function LoginPage() {
     }
 
     const emailValidation = () => {
-             if(formState.email.length < 4){
-                setErrorState('Email Should be at least 4 characters long!')
-             }else{
-                setErrorState('')
-             }
+        if (formState.email.length < 4) {
+            setErrorState('Email should be at least 4 characters long!')
+        } else {
+            setErrorState('')
+        }
+    }
+
+
+    const passwordValidation = () => {
+        if (formState.password.length < 6){
+            setErrorState('Password should be at least 6 characters long!')
+        }else{
+            setErrorState('')
+        }
     }
 
 
     return (
         <section id="login-page" className={styles.loginSection}>
-        <div className={styles.loginContainer}>
-            <form className={styles.loginForm}>
-                <h2>Login</h2>
-                <ul className={styles.listNoBullet}>
-                    <li>
-                        <label htmlFor="email">Email:</label>
-                        <input onChange={formChangeHandler}
-                            onBlur={emailValidation}
-                            type="text"
-                            className={styles.inputField}
-                            id="email"
-                            name="email"
-                            value={formState.email}
-                        />
-                           {errorState && (
-                            <p>{errorState}</p>
-                        )}
-                    </li>
-                    <li>
-                        <label htmlFor="password">Password:</label>
-                        <input onChange={formChangeHandler}
-                            type="password"
-                            className={styles.inputField}
-                            id="password"
-                            name="password"
-                            value={formState.password}
-                        />
-                    </li>
-                    <li className={styles.centeredBtn}>
-                        <button className={styles.loginButton}>Login</button>
-                    </li>
-                </ul>
-            </form>
-        </div>
-    </section>
+            <div className={styles.loginContainer}>
+                <form className={styles.loginForm}>
+                    <h2>Login</h2>
+                    <ul className={styles.listNoBullet}>
+                        <li>
+                            <label htmlFor="email">Email:</label>
+                            <input onChange={formChangeHandler}
+                                onBlur={emailValidation}
+                                type="text"
+                                className={styles.inputField}
+                                id="email"
+                                name="email"
+                                value={formState.email}
+                            />
+                            {errorState && (
+                                <p>{errorState}</p>
+                            )}
+                        </li>
+                        <li>
+                            <label htmlFor="password">Password:</label>
+                            <input onChange={formChangeHandler}
+                                onBlur={passwordValidation}
+                                type="password"
+                                className={styles.inputField}
+                                id="password"
+                                name="password"
+                                value={formState.password}
+                            />
+                      
+                        </li>
+                        <li className={styles.centeredBtn}>
+                            <button className={styles.loginButton}>Login</button>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </section>
 
     );
 
