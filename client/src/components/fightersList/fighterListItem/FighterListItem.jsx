f74fc59026d920658a7267904da80582c3f3a3f9
antoan-97 <antoan.todorov@abv.bg>
2024-07-14 20:36:31 +0300

    Add backContent css to Fighter card

--- client/src/components/fightersList/fighterListItem/FighterListItem.jsx
+++ client/src/components/fightersList/fighterListItem/FighterListItem.jsx
@@ -1,27 +1,26 @@
-import styles from '../../../styles/Collection.module.css'
-
+import styles from '../../../styles/Collection.module.css';
 import { Link } from 'react-router-dom';
 
 export default function FighterListItem({
-    _id,
-    title,
-    category,
-    imageUrl,
-    description
-}
-
-) {
-    return (
-        <div className={`${styles.flip} ${styles.flipVertical}`}>
-            <div className={styles.flipContainerFront}>
-                <img src={imageUrl} alt="fighter" className={styles.frontImage} />
-            </div>
-            <div className={styles.flipContainerBack}>
-                <h1 className={styles.frontHeading}>Name: {title}</h1>
-                <h2 className={styles.flipContainerBackHeading}>Category: {category} </h2>
-                <p className={styles.flipContainerBackDescription}>Description: {description}  </p>
-                <Link to={`/fighters/${_id}/details`} className={styles.detailsButton}>Details</Link>
-            </div>
+  _id,
+  title,
+  category,
+  imageUrl,
+  description
+}) {
+  return (
+    <div className={`${styles.flip} ${styles.flipVertical}`}>
+      <div className={styles.flipContainerFront}>
+        <img src={imageUrl} alt="fighter" className={styles.frontImage} />
+      </div>
+      <div className={styles.flipContainerBack}>
+        <div className={styles.backContent}>
+          <h1 className={styles.frontHeading}>Name: {title}</h1>
+          <h2 className={styles.flipContainerBackHeading}>Category: {category}</h2>
+          <p className={styles.flipContainerBackDescription}>Description: {description}</p>
+          <Link to={`/fighters/${_id}/details`} className={styles.detailsButton}>Details</Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
