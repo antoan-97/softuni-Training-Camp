c20ce3073197baff3cb2a21443bfc38ac3ebebc2
antoan-97 <antoan.todorov@abv.bg>
2024-07-16 20:47:43 +0300

    Create custom useForm hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,13 +14,17 @@ import FighterDetails from './components/fighter-details/FighterDetails'
 
 function App() {
 
+  const loginSubmitHandler = (values) => {
+    console.log(values);
+  }
+
   return (
     <div className={styles.background}>
       <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/register' element={<RegisterPage />}></Route>
-        <Route path='/login' element={<LoginPage />}></Route>
+        <Route path='/login' element={<LoginPage loginSubmitHandler={loginSubmitHandler} />}></Route>
         <Route path='/create' element={<CreatePage />}></Route>
         <Route path='/fighters-list' element={<FightersList />}></Route>
         <Route path='/fighters/:fighterId/details' element={<FighterDetails />}></Route>
--- client/src/components/loginPage/LoginPage.jsx
+++ client/src/components/loginPage/LoginPage.jsx
@@ -1,79 +1,49 @@
 import { useState } from 'react';
+import useForm from '../../hooks/useForm';
+
 import styles from '../../styles/Login.module.css'
 
 
-export default function LoginPage() {
+export default function LoginPage({
+    loginSubmitHandler,
+}
+) {
 
-
-    const initialFormState = {
+    const { values, onChange, onSubmit } = useForm(loginSubmitHandler,{
         email: '',
         password: '',
-    };
-
-
-    const [formState, setFormState] = useState(initialFormState);
-    // const [errorState, setErrorState] = useState('')
-
-    const formChangeHandler = (e) => {
-        setFormState(state => ({
-            ...state,
-            [e.target.name]: e.target.value,
-        })
-        )
-    }
-   
-    // Use it latter for register form
-    // const emailValidation = () => {
-    //     if (formState.email.length < 4) {
-    //         setErrorState({email: 'Email should be at least 4 characters long!'})
-    //     } else {
-    //         setErrorState({email:''})
-    //     }
-    // }
-
-
-    // const passwordValidation = () => {
-    //     if (formState.password.length < 6){
-    //         setErrorState({password:'Password should be at least 6 characters long!'})
-    //     }else{
-    //         setErrorState({password:''})
-    //     }
-    // }
+    });
 
 
     return (
         <section id="login-page" className={styles.loginSection}>
             <div className={styles.loginContainer}>
-                <form className={styles.loginForm}>
+                <form onSubmit={onSubmit} className={styles.loginForm}>
                     <h2>Login</h2>
                     <ul className={styles.listNoBullet}>
                         <li>
                             <label htmlFor="email">Email:</label>
-                            <input onChange={formChangeHandler}
+                            <input onChange={onChange}
                                 // onInput={emailValidation}
                                 type="text"
                                 className={styles.inputField}
                                 id="email"
                                 name="email"
-                                value={formState.email}
+                                value={values.email}
                             />
-                            {/* {errorState.email && (
-                                <p>{errorState.email}</p>
-                            )} */}
+
                         </li>
                         <li>
                             <label htmlFor="password">Password:</label>
-                            <input onChange={formChangeHandler}
+                            <input onChange={onChange}
                                 // onBlur={passwordValidation}
                                 type="password"
                                 className={styles.inputField}
                                 id="password"
                                 name="password"
-                                value={formState.password}
+                                value={values.password}
                             />
-                      {/* {errorState.password && (
-                                <p>{errorState.password}</p>
-                            )} */}
+
                         </li>
                         <li className={styles.centeredBtn}>
                             <button className={styles.loginButton}>Login</button>
