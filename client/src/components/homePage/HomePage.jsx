2391958bcce3c3bad020828cfa91fa822dbee477
antoan-97 <antoan.todorov@abv.bg>
2024-08-01 19:54:49 +0300

    Add button functionality on home page

--- client/src/components/homePage/HomePage.jsx
+++ client/src/components/homePage/HomePage.jsx
@@ -1,10 +1,16 @@
+import { Link } from 'react-router-dom';
 import styles from '../../styles/Home.module.css'
-
 export default function HomePage() {
     return (
+
         <div className={styles.background}>
             <h1 className={styles.homepageTitle}> Welcome to the UFC Fighters Training Camp</h1>
             <h2 className={styles.homepageSubtitle}>Get ready to train like a champion!</h2>
+
+            <div className={styles.buttonContainer}>
+                <Link to={'/fighters-list'} className={styles.getStartedButton}>Get Started!</Link>
+            </div>
+
         </div>
     );
 }
