e4b29886b02944f6112cdb5e3f29e9ec95ace5c2
antoan-97 <antoan.todorov@abv.bg>
2024-08-02 15:19:32 +0300

    Update home page style

--- client/src/components/homePage/HomePage.jsx
+++ client/src/components/homePage/HomePage.jsx
@@ -2,15 +2,14 @@ import { Link } from 'react-router-dom';
 import styles from '../../styles/Home.module.css'
 export default function HomePage() {
     return (
-
         <div className={styles.background}>
-            <h1 className={styles.homepageTitle}> Welcome to the UFC Fighters Training Camp</h1>
-            <h2 className={styles.homepageSubtitle}>Get ready to train like a champion!</h2>
-
-            <div className={styles.buttonContainer}>
+            <div className={styles.content}>
+                <h1 className={styles.homepageTitle}>Welcome to UFC Fighters Training Camp</h1>
+                <p className={styles.homepageSubtitle}>Train with the best and become the best.</p>
+                <div className={styles.buttonContainer}>
                 <Link to={'/fighters-list'} className={styles.getStartedButton}>Get Started!</Link>
+                </div>
             </div>
-
         </div>
     );
 }
