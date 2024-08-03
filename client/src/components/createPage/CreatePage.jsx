957e4e17086f70f706145795ea42913aae21f275
antoan-97 <antoan.todorov@abv.bg>
2024-08-03 15:42:47 +0300

    Validate create form

--- client/src/components/createPage/CreatePage.jsx
+++ client/src/components/createPage/CreatePage.jsx
@@ -2,6 +2,41 @@ import styles from '../../styles/Create.module.css'
 
 import { create } from '../../services/fighterService';
 import { useNavigate } from 'react-router-dom';
+const validateInputs = (data) => {
+  if (!data.title ||
+    !data.category ||
+    !data.imageUrl ||
+    !data.wins ||
+    !data.loses ||
+    !data.weight ||
+    !data.description) {
+    notifyError('Please fill in all fields.');
+    return false;
+  }
+
+
+  const wins = Number(data.wins);
+  const loses = Number(data.loses);
+  const weight = Number(data.weight);
+
+  if (isNaN(wins) || isNaN(loses) || isNaN(weight)) {
+    notifyError('Wins, loses, and weight must be valid numbers.');
+    return false;
+  }
+
+  if (data.wins < 0 || data.loses < 0 || data.weight < 0) {
+    notifyError('Please select a value that is not less than 0 ');
+    return false
+  }
+
+  const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
+  if (!urlPattern.test(data.imageUrl)) {
+    notifyError('Please enter a valid URL for the image.');
+    return false;
+  }
+
+  return true;
+};
 
 
 
